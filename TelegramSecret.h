53551eb86b946d377c10abba30a2efcded3a1cd5
RTA <reversethatapp@gmail.com>
2023-08-08 10:49:04 +0800

    - ignore telegram secret changes

--- TelegramSecret.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// Replace with your actual Telegram bot token
-// how to get token: Add @BotFather (official Telegram bot) and generate new token from there
-std::string TELEGRAM_BOT_TOKEN = "";
-
-// Make this curl to get chat id: curl https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/getUpdates
-std::string TELEGRAM_CHAT_ID = "";
